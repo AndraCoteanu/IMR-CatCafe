3ad13814cc5f1ab8237d3ee88d7d63242fa43f83
Iustin07 <daniel.bodnariu@info.uaic.ro>
2021-12-07 19:05:48 +0200

    menu->mainscene

---
+++ CatCafe/Assets/Scripts/Menu_Scripts/QuitButton.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        exitGame();
+    }
+
+    // Update is called once per frame
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
